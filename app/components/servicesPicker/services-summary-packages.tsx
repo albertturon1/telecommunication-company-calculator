7b2b801cd033349b696495e1185372ec8edea987
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-12 15:44:45 +0200

    feat:created RemoveYearAlertDialog; fixed UI conditions in services-summary-packages

--- app/components/servicesPicker/services-summary-packages.tsx
+++ app/components/servicesPicker/services-summary-packages.tsx
@@ -1,8 +1,9 @@
-import { DEFAULT_CURRENCY } from "@components/constants/globals";
 import ServicesSummary, {
   ServicesSummaryRow,
 } from "@components/misc/services-summary";
+import { DEFAULT_CURRENCY } from "@constants/globals";
 import { Package } from "@interfaces/IPricing";
+import { cn } from "@src/lib/utils";
 
 const ServicesSummaryWithPackages = ({
   remainingPrice,
@@ -20,37 +21,54 @@ const ServicesSummaryWithPackages = ({
     regularPriceTitle={
       packages && packages.length > 0 ? "Regular price" : "Price"
     }
+    containerClassName="px-4"
   >
-    {packages && packages.length > 0 && discountedPrice > 0 && (
+    {packages && (
       <div className="flex flex-col gap-y-1">
-        <ServicesSummaryRow
-          rightTextClassName="text-discount"
-          leftText={"Discounted price"}
-          rightText={`${discountedPrice} ${DEFAULT_CURRENCY}`}
-        />
-        {/* show extra information about packages */}
-        {(packages.length > 1 || remainingPrice > 0) && (
-          <>
-            {packages.map((bundlePackage) => (
-              <div
-                className="flex justify-between items-center text-black/80 text-xs gap-x-1"
-                key={bundlePackage.package_id}
-              >
-                <p>{bundlePackage.name}</p>
-                <p className="font-semibold min-w-[50px] text-end">{`${bundlePackage.price} ${DEFAULT_CURRENCY}`}</p>
-              </div>
-            ))}
-            {remainingPrice > 0 && (
-              <div className="flex justify-between items-center text-black/80 text-xs gap-x-1">
-                <p>{"Rest"}</p>
-                <p className="font-semibold min-w-[50px] text-end">{`${remainingPrice} ${DEFAULT_CURRENCY}`}</p>
-              </div>
-            )}
-          </>
+        {packages.length > 0 && (
+          <ServicesSummaryRow
+            rightTextClassName="text-discount"
+            leftText={"Discounted price"}
+            rightText={`${discountedPrice} ${DEFAULT_CURRENCY}`}
+          />
+        )}
+        {packages.length > 1 &&
+          packages.map((bundlePackage) => (
+            <DiscountRow
+              key={bundlePackage.package_id}
+              leftText={bundlePackage.name}
+              rightText={`${bundlePackage.price} ${DEFAULT_CURRENCY}`}
+            />
+          ))}
+        {remainingPrice > 0 && (
+          <DiscountRow
+            leftText={"Rest"}
+            rightText={`${remainingPrice} ${DEFAULT_CURRENCY}`}
+          />
         )}
       </div>
     )}
   </ServicesSummary>
+);
+
+const DiscountRow = ({
+  className,
+  leftText,
+  rightText,
+}: {
+  className?: string;
+  leftText: string;
+  rightText: string | number;
+}) => (
+  <div
+    className={cn(
+      "flex justify-between items-center text-black/80 text-xs gap-x-1",
+      className
+    )}
+  >
+    <p>{leftText}</p>
+    <p className={"font-semibold min-w-[50px] text-end"}>{rightText}</p>
+  </div>
 );
 
 export default ServicesSummaryWithPackages;
