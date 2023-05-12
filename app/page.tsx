4ee557c3547d4830b7e09dc7e29f57fa2e9dccca
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-12 15:38:39 +0200

    fix: fixed errors on production with loading data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 import { ShopResponse } from "@interfaces/IPricing";
 
 import Landing from "./components/landing";
@@ -7,9 +10,14 @@ import PageClient from "./page-client";
 export const revalidate = 0;
 
 async function getPricelist() {
-  return (await fetch(`http://localhost:3000/pricing.json`).then((res) =>
-    res.json()
-  )) as ShopResponse | undefined;
+  const jsonDirectory = path.join(process.cwd(), "json");
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(
+    `${jsonDirectory}/pricing.json`,
+    "utf8"
+  );
+
+  return JSON.parse(fileContents) as ShopResponse;
 }
 
 export default async function Home() {
