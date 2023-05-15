53f6228af83edc5915891208e40ba2aa1df0cbd1
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-15 02:04:20 +0200

    fix: replace YearSelect with react-select; remove @radix-ui/react-label and @radix-ui/react-select; added Skeleton

--- app/components/AllServicesSummary.tsx
+++ app/components/AllServicesSummary.tsx
@@ -16,7 +16,7 @@ const AllServicesSummary = ({
   pricingYears: PricingYear[];
   bundleYears: BundleYear[];
 }) => {
-  const allYearsSummary = selectedServices.map((y) =>
+  const separateSummariesForYears = selectedServices.map((y) =>
     summarizePackagesForYear({
       selectedYear: y.year,
       pricingYears,
@@ -25,7 +25,7 @@ const AllServicesSummary = ({
     })
   );
 
-  const c = allYearsSummary.reduce(
+  const allYearsSummary = separateSummariesForYears.reduce(
     (acc, item) => {
       if (!item) return acc;
       acc.remainingPrice += item.remainingPrice;
@@ -40,20 +40,22 @@ const AllServicesSummary = ({
     <div
       className={cn(
         "fixed z-10 w-full pt-4 pb-6 flex flex-col border-t border-border bg-muted transition-all duration-150",
-        c.regularPrice === 0 ? "-bottom-96" : "bottom-0"
+        allYearsSummary.regularPrice === 0 ? "-bottom-96" : "bottom-0"
       )}
     >
       <div className="container max-w-xl mx-auto flex flex-col gap-y-4">
         <h1 className="text-lg font-medium">{"Summary of services pricing"}</h1>
         <ServicesSummary
-          {...c}
-          regularPriceTitle={c.discountedPrice > 0 ? "Regular price" : "Price"}
+          {...allYearsSummary}
+          regularPriceTitle={
+            allYearsSummary.discountedPrice > 0 ? "Regular price" : "Price"
+          }
         >
           <ServicesSummaryRow
             rightTextClassName="text-discount"
-            visible={c.discountedPrice > 0}
+            visible={allYearsSummary.discountedPrice > 0}
             leftText={"Discounted price"}
-            rightText={`${c.discountedPrice} ${DEFAULT_CURRENCY}`}
+            rightText={`${allYearsSummary.discountedPrice} ${DEFAULT_CURRENCY}`}
           />
         </ServicesSummary>
       </div>
